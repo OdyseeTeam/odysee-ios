abd42760d05940e08dac50160e3a72c8e416ae27
Akinwale Ariwodola <akinwale@gmail.com>
2021-02-25 13:30:51 +0100

    YT channel claiming

--- Odysee/Controllers/User/YouTubeSyncStatusViewController.swift
+++ Odysee/Controllers/User/YouTubeSyncStatusViewController.swift
@@ -16,12 +16,14 @@ class YouTubeSyncStatusViewController: UIViewController {
     @IBOutlet weak var cmStatusClaimYourChannel: UIImageView!
     
     @IBOutlet weak var claimChannelButton: UIButton!
+    @IBOutlet weak var exploreOdyseeButton: UIButton!
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     
     @IBOutlet weak var claimHandleLabel: UILabel!
     @IBOutlet weak var syncVideoStat: UILabel!
     
     var timer: Timer = Timer()
+    var currentChannel: String? = nil
     let timerInterval: Double = 60 // 1 minute
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,10 +54,96 @@ class YouTubeSyncStatusViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(self.fetchSyncStatus), userInfo: nil, repeats: true)
     }
     
-    @IBAction func claimChannelTapped(_ sender: UIButton) {
-        
+    func restoreButtons() {
+        DispatchQueue.main.async {
+            self.claimChannelButton.isEnabled = true
+            self.claimChannelButton.isHidden = false
+            self.exploreOdyseeButton.isHidden = false
+            self.loadingIndicator.isHidden = true
+        }
     }
     
+    @IBAction func claimChannelTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+        sender.isHidden = true
+        exploreOdyseeButton.isHidden = true
+        loadingIndicator.isHidden = false
+        
+        Lbry.apiCall(method: "address_list", params: [:], connectionString: Lbry.lbrytvConnectionString, authToken: Lbryio.authToken, completion: { data, error in
+            guard let data = data, error == nil else {
+                self.showError(error: error)
+                self.restoreButtons()
+                return
+            }
+            
+            if let result = data["result"] as? [String: Any] {
+                if let items = result["items"] as? [[String: Any]] {
+                    if items.count > 0 {
+                        let address = items[0]["address"] as! String
+                        let publicKey = items[0]["pubkey"] as! String
+                        self.transferChannel(address: address, publicKey: publicKey)
+                        return
+                    }
+                }
+            }
+            
+            self.showError(message: "The channel could not be claimed. Please email help@odysee.com for support.")
+            self.restoreButtons()
+        })
+    }
+    
+    func transferChannel(address: String, publicKey: String) {
+        do {
+            let options = ["address": address, "public_key": publicKey]
+            try Lbryio.call(resource: "yt", action: "transfer", options: options, method: Lbryio.methodPost, completion: { data, error in
+                guard let data = data, error == nil else {
+                    self.showError(error: error)
+                    self.restoreButtons()
+                    return
+                }
+                if let channelList = data as? [[String: Any]] {
+                    if channelList.count > 0 {
+                        for item in channelList {
+                            if let channelData = item["channel"] as? [String: Any] {
+                                if channelData["lbry_channel_name"] as? String == self.currentChannel {
+                                    if let channelCert = channelData["channel_certificate"] as? String {
+                                        self.importChannel(channelCert)
+                                        return
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                self.showError(message: "The channel could not be claimed at this time. Please email help@odysee.com for support.")
+                self.restoreButtons()
+            })
+        } catch let error {
+            self.showError(error: error)
+            self.restoreButtons()
+        }
+    }
+    
+    func importChannel(_ channelCert: String) {
+        Lbry.apiCall(method: "channel_import", params: ["channel_data": channelCert], connectionString: Lbry.lbrytvConnectionString, authToken: Lbryio.authToken, completion: { data, error in
+            guard let _ = data, error == nil else {
+                self.showError(error: error)
+                self.restoreButtons()
+                return
+            }
+            
+            self.finishClaimChannel()
+        })
+    }
+    
+    func finishClaimChannel() {
+        DispatchQueue.main.async {
+            self.showMessage(message: "You have successfully claimed your YouTube channel")
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.mainNavigationController?.popViewController(animated: true)
+        }
+    }
     @IBAction func exploreOdyseeTapped(_ sender: UIButton) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         appDelegate.mainNavigationController?.popViewController(animated: true)
@@ -90,20 +178,22 @@ class YouTubeSyncStatusViewController: UIViewController {
             let totalSubs = channelData["total_subs"] as? Int
             let totalVideos = channelData["total_videos"] as? Int
             let transferState = channelData["transfer_state"] as? String
-            let transferable = channelData["transferable"] as? Bool
+            //let transferable = channelData["transferable"] as? Bool
             let syncStatus = channelData["sync_status"] as? String
             
+            self.currentChannel = lbryChannelName
             self.claimHandleLabel.text = String(format: String.localized("Claim your handle %@"), lbryChannelName ?? "")
             self.syncVideoStat.text = String(format: String.localized("Syncing %d video(s) from your channel with %d subscription(s)."),
                                         totalVideos ?? 0, totalSubs ?? 0)
             self.cmStatusClaimYourHandle.image = UIImage.init(systemName: "checkmark.circle")
             self.cmStatusAgreeToSync.image = UIImage.init(systemName: "checkmark.circle")
-            if syncStatus == "done" {
+            if syncStatus == "synced" {
                 self.cmStatusWaitForVideos.image = UIImage.init(systemName: "checkmark.circle")
-                if transferable ?? false && transferState != "transferred" {
-                    self.claimChannelButton.isEnabled = true
-                }
+                //if transferable ?? false && transferState != "transferred" {
+                self.claimChannelButton.isEnabled = true
+                //}
             }
+            
             if transferState == "transferred" {
                 self.cmStatusClaimYourChannel.image = UIImage.init(systemName: "checkmark.circle")
             }
