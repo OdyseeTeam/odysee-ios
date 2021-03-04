67e6053c51196d58b57ddc3b24aaea6cea718ade
Akinwale Ariwodola <akinwale@gmail.com>
2021-03-04 20:27:20 +0100

    publishing

---
+++ Odysee/Controllers/Library/PublishViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PublishViewController.swift
+//  Odysee
+//
+//  Created by Akinwale Ariwodola on 26/02/2021.
+//
+
+import UIKit
+
+class PublishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
