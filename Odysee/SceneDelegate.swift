6f75b1d2796a42c0a5e54f1a5130b5d1e31d1dba
Akinwale Ariwodola <akinwale@gmail.com>
2021-05-28 11:00:10 +0100

    reconnect player when scene becomes active

--- Odysee/SceneDelegate.swift
+++ Odysee/SceneDelegate.swift
@@ -42,6 +42,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        if appDelegate.currentFileViewController != nil {
+            appDelegate.currentFileViewController?.connectPlayer()
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
