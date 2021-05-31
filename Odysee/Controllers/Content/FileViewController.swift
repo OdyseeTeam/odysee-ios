cd1f68313d40b7fe638f96cdf0087378e9a56755
Adlai Holler <adlai@google.com>
2021-05-31 05:49:22 -0400

    Migrate HomeViewController to new apiCall method (#140)
    * Migrate HomeViewController to new apiCall method
    * Make the API type-safe

--- Odysee/Controllers/Content/FileViewController.swift
+++ Odysee/Controllers/Content/FileViewController.swift
@@ -777,7 +777,7 @@ class FileViewController: UIViewController, UIGestureRecognizerDelegate, UINavig
                 }
             }
             
-            Lbry.apiCall(method: Lbry.methodResolve,
+            Lbry.apiCall(method: Lbry.Methods.resolve,
                          params: ["urls": resolveUrls],
                          url: Lbry.lbrytvURL,
                          completion: self.handleRelatedContentResult)
