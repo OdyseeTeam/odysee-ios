edb8368491145e60fccd0174b3c5bc31c63f579f
Kyle Roucis <kroucis@users.noreply.github.com>
2021-07-24 04:14:32 -0400

    [#31] Improve the UX flow for the Log In mode switch button at the bo… (#224)
    * [#31] Change the "Log In/Sign Up" button width to be half of the primary "Create Account" button and similar controls. Also move the `masksToBounds` and `cornerRadius` attribute values into the Storyboard file.
    
    Co-authored-by: Grimless <kroucis@bigbytestudios.com>

--- Odysee/Controllers/User/UserAccountViewController.swift
+++ Odysee/Controllers/User/UserAccountViewController.swift
@@ -18,6 +18,7 @@ class UserAccountViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var defaultActionButton: UIButton!
     @IBOutlet weak var controlsStackView: UIStackView!
+    @IBOutlet weak var switchModeButton: UIButton!
     
     @IBOutlet weak var verificationLabel: UILabel!
     @IBOutlet weak var agreementLabel: UILabel!
@@ -196,7 +197,8 @@ class UserAccountViewController: UIViewController {
     }
     
     func enableSignInMode() {
-        haveAccountLabel.text = String.localized("Don't have an account? Sign Up.")
+        haveAccountLabel.text = String.localized("Don't have an account?")
+        switchModeButton.setTitle(String.localized("Sign Up"), for: .normal)
         titleLabel.text = String.localized("Log In to Odysee")
         defaultActionButton.setTitle(String.localized("Continue"), for: .normal)
         
@@ -207,7 +209,8 @@ class UserAccountViewController: UIViewController {
     }
     
     func enableSignUpMode() {
-        haveAccountLabel.text = String.localized("Already have an account? Log In.")
+        haveAccountLabel.text = String.localized("Already have an account?")
+        switchModeButton.setTitle(String.localized("Log In"), for: .normal)
         titleLabel.text = String.localized("Join Odysee")
         defaultActionButton.setTitle(String.localized("Sign Up"), for: .normal)
         
