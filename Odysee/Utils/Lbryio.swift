OdyseeTeam/odysee-ios

commit dbcdd9419b3e45f25484ba13b5dbcb97626b215a

fix exchange rate retrieval crash bug

Odysee/Utils/Lbryio.swift
@@ -440,10 +440,9 @@ final class Lbryio {
                     return
                 }
 
-                let response = data as! [String: Any]
-                let lbcUsdRate = response["lbc_usd"] as? Double
-                if lbcUsdRate != nil {
-                    currentLbcUsdRate = Decimal(lbcUsdRate!)
+                if let response = data as? [String: Any],
+                   let lbcUsdRate = response["lbc_usd"] as? Double {
+                    currentLbcUsdRate = Decimal(lbcUsdRate)
                     completion(currentLbcUsdRate, nil)
                     return
                 }

commit c3a14bc004b82e5b4116d3a0f0605b2fa4dc2a45

Fix lint error caused by dbcdd94

Odysee/Utils/Lbryio.swift
@@ -441,7 +441,8 @@ final class Lbryio {
                 }
 
                 if let response = data as? [String: Any],
-                   let lbcUsdRate = response["lbc_usd"] as? Double {
+                   let lbcUsdRate = response["lbc_usd"] as? Double
+                {
                     currentLbcUsdRate = Decimal(lbcUsdRate)
                     completion(currentLbcUsdRate, nil)
                     return