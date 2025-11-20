OdyseeTeam/odysee-ios

commit 50fd1fa6ce251bc17c5daef73a1d6a3d32bfa184

Handle arbitrary comment reply depth
Each reply layer adds 16px to comment leading spacing

Naively walk up to root for each comment
O(n^2) operation, can be optimized with cache

Odysee/Controllers/Content/CommentsViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Akinwale Ariwodola on 11/01/2021.
 //
 
+import Combine
 import UIKit
 
 class CommentsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDataSource,
@@ -46,6 +47,7 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
     var currentCommentAsIndex = -1
 
     // From notification
+    var currentCommentIsReply: Bool = false
     var currentCommentId: String?
     var hasScrolledToCurrentComment: Bool = false
 
@@ -61,6 +63,9 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
         }
         if comments.count == 0, !commentsDisabled {
             loadComments()
+        }
+        if currentCommentIsReply, currentCommentId != nil {
+            loadCurrentCommentThread()
         }
     }
 
@@ -245,25 +250,27 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
                 self.commentList.reloadData()
                 self.checkNoComments()
 
-                if self.currentCommentId != nil {
+                if self.currentCommentId != nil && !self.currentCommentIsReply {
                     if !self.commentsLastPageReached && !self.comments.contains(where: {
                         $0.commentId == self.currentCommentId
                     }) {
                         self.commentsCurrentPage += 1
                         self.loadComments()
                     } else if !self.hasScrolledToCurrentComment {
-                        if let currentCommentIndex = self.comments.firstIndex(where: {
-                            $0.commentId == self.currentCommentId
-                        }) {
-                            let indexPath = IndexPath(row: currentCommentIndex, section: 0)
-                            self.commentList.scrollToRow(at: indexPath, at: .top, animated: true)
-                            self.hasScrolledToCurrentComment = true
-                        } else {
-                            self.showError(message: String.localized("Comment could not be found"))
-                        }
+                        self.scrollToCurrentComment()
                     }
                 }
             }
+        }
+    }
+
+    func scrollToCurrentComment() {
+        if let currentCommentIndex = comments.firstIndex(where: { $0.commentId == currentCommentId }) {
+            let indexPath = IndexPath(row: currentCommentIndex, section: 0)
+            commentList.scrollToRow(at: indexPath, at: .top, animated: true)
+            hasScrolledToCurrentComment = true
+        } else {
+            showError(message: String.localized("Comment could not be found"))
         }
     }
 
@@ -400,19 +407,31 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
             switch result {
             case let .failure(error):
                 self.showError(error: error)
-            case let .success(comment):
+            case var .success(comment):
                 if let currentReplyToComment = self.currentReplyToComment {
                     if currentReplyToComment.repliesLoaded ?? false {
                         if let parentIndex = self.comments.firstIndex(where: {
                             $0.commentId == currentReplyToComment.commentId
                         }) {
+                            var currentComment: Comment? = comment
+                            while let _currentComment = currentComment {
+                                comment.replyDepth += 1
+                                currentComment = self.comments.first(where: {
+                                    $0.commentId == _currentComment.parentId
+                                })
+                            }
                             self.comments.insert(comment, at: parentIndex + 1)
+                            self.commentList.insertRows(
+                                at: [IndexPath(row: parentIndex + 1, section: 0)],
+                                with: .automatic
+                            )
                         }
                     } else {
                         self.loadReplies(currentReplyToComment)
                     }
                 } else {
                     self.comments.insert(comment, at: 0)
+                    self.commentList.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
                 }
                 // TODO: Reactions not loaded here as comment might not be available?
                 self.resolveCommentAuthors(urls: [comment.channelUrl!])
@@ -423,7 +442,6 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
                 self.replyToContainerView.isHidden = true
                 self.loadingContainer.isHidden = true
                 self.textViewDidChange(self.commentInput)
-                self.commentList.reloadData()
                 self.checkNoComments()
 
                 self.currentReplyToComment = nil
@@ -651,6 +669,14 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
             case let .success(page):
                 let loadedComments = page.items.filter {
                     comment in !self.comments.contains(where: { $0.commentId == comment.commentId })
+                }.map { comment in
+                    var comment = comment
+                    var currentComment: Comment? = comment
+                    while let _currentComment = currentComment {
+                        comment.replyDepth += 1
+                        currentComment = self.comments.first(where: { $0.commentId == _currentComment.parentId })
+                    }
+                    return comment
                 }
 
                 if let parentIndex = self.comments.firstIndex(where: {
@@ -670,6 +696,111 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
                 self.commentsLoading = false
                 self.setCommentRepliesLoaded(parent)
                 self.loadingContainer.isHidden = true
+            }
+        }
+    }
+
+    func loadCurrentCommentThread() {
+        loadingContainer.isHidden = false
+        Lbry.commentApiCall(
+            method: Lbry.CommentMethods.byId,
+            params: .init(
+                commentId: currentCommentId!,
+                withAncestors: true
+            )
+        )
+        .subscribeResult { result in
+            switch result {
+            case let .failure(error):
+                self.showError(error: error)
+            case let .success(result):
+                if let ancestors = result.ancestors {
+                    if let parent = ancestors.last {
+                        if !self.comments.contains(where: { $0.commentId == parent.commentId }) {
+                            self.comments.insert(parent, at: 0)
+                            self.commentList.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+                        }
+                    }
+                    self.loadThread(thread: ancestors.reversed())
+                }
+            }
+        }
+    }
+
+    func loadThread(thread: [Comment]) {
+        thread.publisher.subscribe(on: DispatchQueue.global()).tryMap { comment -> (Comment, URLRequest) in
+            (comment, try Lbry.apiRequest(
+                method: Lbry.CommentMethods.list.name,
+                params: .init(
+                    claimId: self.claimId!,
+                    parentId: comment.commentId!,
+                    page: 1,
+                    pageSize: 999,
+                    skipValidation: true,
+                    topLevel: false
+                ) as CommentListParams,
+                url: Lbry.commentronURL,
+                authToken: Lbryio.authToken
+            ))
+        }
+        .flatMap(maxPublishers: .max(1)) { comment, request in
+            // Run data task.
+            URLSession.shared.dataTaskPublisher(for: request).mapError { $0 as Error }.map { (comment, $0) }
+        }
+        .tryMap { comment, dataTaskOutput -> (Comment, Page<Comment>) in
+            let (data, _) = dataTaskOutput
+
+            // Decode and validate result.
+            let response = try JSONDecoder().decode(Lbry.APIResponse<Page<Comment>>.self, from: data)
+            if response.jsonrpc != "2.0" {
+                assertionFailure()
+                throw LbryApiResponseError("wrong jsonrpc \(response.jsonrpc)")
+            }
+
+            guard let result = response.result else {
+                throw LbryApiResponseError(response.error?.message ?? "unknown api error")
+            }
+            return (comment, result)
+        }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+        .subscribeResultFinally { result in
+            switch result {
+            case let .failure(error):
+                self.showError(error: error)
+            case let .success(output):
+                if let (parent, page) = output {
+                    let loadedComments = page.items.filter {
+                        comment in !self.comments.contains(where: { $0.comment == comment.commentId })
+                    }.map { comment in
+                        var comment = comment
+                        var currentComment: Comment? = comment
+                        while let _currentComment = currentComment {
+                            comment.replyDepth += 1
+                            currentComment = self.comments.first(where: { $0.commentId == _currentComment.parentId })
+                        }
+                        return comment
+                    }
+
+                    if let parentIndex = self.comments.firstIndex(where: {
+                        $0.commentId == parent.commentId
+                    }), loadedComments.count > 0 {
+                        self.comments.insert(contentsOf: loadedComments, at: parentIndex + 1)
+                        self.commentList.insertRows(
+                            at: Array(parentIndex + 1 ... parentIndex + loadedComments.count).map {
+                                IndexPath(row: $0, section: 0)
+                            },
+                            with: .automatic
+                        )
+                        self.loadCommentReactions(commentIds: loadedComments.map(\.commentId!))
+                        self.resolveCommentAuthors(urls: loadedComments.map(\.channelUrl!))
+                    }
+
+                    self.setCommentRepliesLoaded(parent)
+                    self.loadingContainer.isHidden = true
+                } else {
+                    self.scrollToCurrentComment()
+                }
             }
         }
     }

Odysee.xcodeproj/project.pbxproj
@@ -11,7 +11,7 @@
 		532650E4286EAB1B00F0FF0B /* Settings.bundle in Resources */ = {isa = PBXBuildFile; fileRef = 532650E3286EAB1B00F0FF0B /* Settings.bundle */; };
 		534B71A2287BE08D00F01D42 /* CommentAPIParams.swift in Sources */ = {isa = PBXBuildFile; fileRef = 534B71A1287BE08D00F01D42 /* CommentAPIParams.swift */; };
 		534B71A4287BE0F200F01D42 /* ChannelSignResult.swift in Sources */ = {isa = PBXBuildFile; fileRef = 534B71A3287BE0F200F01D42 /* ChannelSignResult.swift */; };
-		53593F31287E5D75007FD08E /* ReactListResult.swift in Sources */ = {isa = PBXBuildFile; fileRef = 53593F30287E5D75007FD08E /* ReactListResult.swift */; };
+		53593F31287E5D75007FD08E /* CommentAPIResult.swift in Sources */ = {isa = PBXBuildFile; fileRef = 53593F30287E5D75007FD08E /* CommentAPIResult.swift */; };
 		5385092028E13D28009A7AAB /* MembershipPerk.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5385091F28E13D28009A7AAB /* MembershipPerk.swift */; };
 		53AED4F32812A8750088A1BA /* OdyseeLivestream.swift in Sources */ = {isa = PBXBuildFile; fileRef = 53AED4F22812A8750088A1BA /* OdyseeLivestream.swift */; };
 		53BC58FC2818DF530017F9D2 /* LivestreamCollectionViewCell.xib in Resources */ = {isa = PBXBuildFile; fileRef = 53BC58FB2818DF530017F9D2 /* LivestreamCollectionViewCell.xib */; };
@@ -142,7 +142,7 @@
 		532650E3286EAB1B00F0FF0B /* Settings.bundle */ = {isa = PBXFileReference; lastKnownFileType = "wrapper.plug-in"; path = Settings.bundle; sourceTree = "<group>"; };
 		534B71A1287BE08D00F01D42 /* CommentAPIParams.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CommentAPIParams.swift; sourceTree = "<group>"; };
 		534B71A3287BE0F200F01D42 /* ChannelSignResult.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ChannelSignResult.swift; sourceTree = "<group>"; };
-		53593F30287E5D75007FD08E /* ReactListResult.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ReactListResult.swift; sourceTree = "<group>"; };
+		53593F30287E5D75007FD08E /* CommentAPIResult.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CommentAPIResult.swift; sourceTree = "<group>"; };
 		5385091F28E13D28009A7AAB /* MembershipPerk.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MembershipPerk.swift; sourceTree = "<group>"; };
 		53AED4F22812A8750088A1BA /* OdyseeLivestream.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OdyseeLivestream.swift; sourceTree = "<group>"; };
 		53BC58FB2818DF530017F9D2 /* LivestreamCollectionViewCell.xib */ = {isa = PBXFileReference; lastKnownFileType = file.xib; path = LivestreamCollectionViewCell.xib; sourceTree = "<group>"; };
@@ -323,13 +323,13 @@
 				CC015767267C1A0B00F96EFE /* ResolveResult.swift */,
 				CC3A1E7726853D7E00683E28 /* APIParams.swift */,
 				534B71A1287BE08D00F01D42 /* CommentAPIParams.swift */,
+				53593F30287E5D75007FD08E /* CommentAPIResult.swift */,
 				641E7EC02608BE0C00A79CD3 /* Language.swift */,
 				CC80EC0E2672A80700CA07DA /* Page.swift */,
 				641E7EC52608BE5700A79CD3 /* License.swift */,
 				641B3E2627E0EFCC0089E816 /* CustomBlockRule.swift */,
 				6402A30227E14C8800113E26 /* OdyseeLocale.swift */,
 				534B71A3287BE0F200F01D42 /* ChannelSignResult.swift */,
-				53593F30287E5D75007FD08E /* ReactListResult.swift */,
 			);
 			path = Models;
 			sourceTree = "<group>";
@@ -800,7 +800,7 @@
 				CC97807926549867009F580E /* Log.swift in Sources */,
 				64FBE79D25502A290082AF2E /* AppDelegate.swift in Sources */,
 				5385092028E13D28009A7AAB /* MembershipPerk.swift in Sources */,
-				53593F31287E5D75007FD08E /* ReactListResult.swift in Sources */,
+				53593F31287E5D75007FD08E /* CommentAPIResult.swift in Sources */,
 				6413273E2556E86000CBB6B8 /* Claim.swift in Sources */,
 				642B6B0C257AB9F200F17355 /* WalletBalance.swift in Sources */,
 				646E54EA257E565000BEBFBE /* SupportViewController.swift in Sources */,

Odysee/Base.lproj/Main.storyboard
@@ -8249,7 +8249,6 @@
                                             <outlet property="fireReactionImage" destination="Oq4-95-IdW" id="Orb-vS-wRC"/>
                                             <outlet property="fireReactionLabel" destination="swc-uj-cgw" id="dRk-N3-zuB"/>
                                             <outlet property="leadingLayoutConstraint" destination="uyH-mY-yxV" id="4bj-cj-b2T"/>
-                                            <outlet property="replyButton" destination="1Tf-7t-6WT" id="hhF-Qf-f7k"/>
                                             <outlet property="replyCountButton" destination="cFU-cI-xhg" id="tUm-jk-ANx"/>
                                             <outlet property="reportContentContainer" destination="fmC-kO-ISw" id="BTl-c3-ksh"/>
                                             <outlet property="slimeReactionContainer" destination="kEo-fG-5kP" id="qYG-fM-RzW"/>

Odysee/Controllers/Content/FileViewController.swift
@@ -154,6 +154,7 @@ class FileViewController: UIViewController, UIGestureRecognizerDelegate, UINavig
     var commentAsPicker: UIPickerView!
     var currentCommentAsIndex = -1
     // From notification
+    var currentCommentIsReply: Bool = false
     var currentCommentId: String?
 
     var numLikes = 0
@@ -1805,6 +1806,7 @@ class FileViewController: UIViewController, UIGestureRecognizerDelegate, UINavig
         commentsVc.commentsDisabled = commentsDisabled
         commentsVc.comments = comments.elements
         commentsVc.currentCommentId = currentCommentId
+        commentsVc.currentCommentIsReply = currentCommentIsReply
         commentsVc.authorThumbnailMap = authorThumbnailMap
         commentsVc.commentsLastPageReached = commentsLastPageReached
 

Odysee/Controllers/User/NotificationsViewController.swift
@@ -292,8 +292,10 @@ class NotificationsViewController: UIViewController, UIGestureRecognizerDelegate
                 } else {
                     let vc = storyboard?.instantiateViewController(identifier: "file_view_vc") as! FileViewController
                     vc.claimUrl = lbryUrl
-                    if notification.notificationParameters?.device?.type == "comment" {
+                    if ["comment", "reply"].contains(notification.notificationParameters?.device?.type) {
                         vc.currentCommentId = notification.notificationParameters?.dynamic?.hash
+                        vc.currentCommentIsReply = !(notification.notificationParameters?.dynamic?.parentId ?? "")
+                            .isEmpty
                     }
                     appDelegate.mainNavigationController?.view.layer.add(
                         Helper.buildFileViewTransition(),

Odysee/Models/Comment.swift
@@ -30,6 +30,8 @@ struct Comment: Decodable, Hashable {
 
     var repliesLoaded: Bool?
 
+    var replyDepth: Int = 1
+
     private enum CodingKeys: String, CodingKey {
         case channelId = "channel_id"
         case channelName = "channel_name"

Odysee/Models/CommentAPIParams.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+struct CommentByIdParams: Encodable {
+    var commentId: String
+    var withAncestors: Bool?
+}
+
 struct CommentListParams: Encodable {
     var claimId: String
     var channelId: String?

Odysee/Models/CommentAPIResult.swift
@@ -1,11 +1,21 @@
 //
-//  ReactListResult.swift
+//  CommentAPIResult.swift
 //  Odysee
 //
 //  Created by Keith Toh on 13/07/2022.
 //
 
 import Foundation
+
+struct CommentByIdResult: Decodable {
+    var comment: Comment?
+    var ancestors: [Comment]?
+
+    enum CodingKeys: String, CodingKey {
+        case comment = "items"
+        case ancestors
+    }
+}
 
 struct ReactListResult: Decodable {
     struct Reaction: Decodable {

commit df6fe0c369f34e9f25647f8c8dad7bda528020de

vc(comments): Fix loadThread deduplication
Fixes: 50fd1fa Handle arbitrary comment reply depth

Odysee/Controllers/Content/CommentsViewController.swift
@@ -771,7 +771,7 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
             case let .success(output):
                 if let (parent, page) = output {
                     let loadedComments = page.items.filter {
-                        comment in !self.comments.contains(where: { $0.comment == comment.commentId })
+                        comment in !self.comments.contains(where: { $0.commentId == comment.commentId })
                     }.map { comment in
                         var comment = comment
                         var currentComment: Comment? = comment