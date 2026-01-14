da3426868fbc53054c8e3b998d1d926c3cca8224
kt programs <ktprograms@gmail.com>
2026-01-14 22:23:59 +0800

    Remove PII analytics

--- Odysee/Controllers/User/UserAccountViewController.swift
+++ Odysee/Controllers/User/UserAccountViewController.swift
@@ -184,7 +184,6 @@ class UserAccountViewController: UIViewController {
                 if let stringData = data as? String {
                     if stringData.lowercased() == "ok" {
                         self.currentEmail = email
-                        Analytics.logEvent("email_added", parameters: ["email": self.currentEmail as Any])
 
                         // display waiting for email verification view
                         self.waitForVerification()
@@ -274,8 +273,6 @@ class UserAccountViewController: UIViewController {
 
                     // close the view
                     DispatchQueue.main.async {
-                        Analytics.logEvent("email_verified", parameters: ["email": self.currentEmail as Any])
-
                         AppDelegate.shared.mainController.checkUploadButton()
 
                         // after email verification, finish with wallet sync
@@ -334,7 +331,6 @@ class UserAccountViewController: UIViewController {
                                 // old email verification flow
                                 self.currentEmail = email
                                 self.handleUserSignInWithoutPassword(email: email)
-                                Analytics.logEvent("email_added", parameters: ["email": self.currentEmail as Any])
                             } else {
                                 DispatchQueue.main.async {
                                     self.defaultActionButton.isEnabled = true
@@ -348,7 +344,6 @@ class UserAccountViewController: UIViewController {
                     }
 
                     self.currentEmail = email
-                    Analytics.logEvent("email_added", parameters: ["email": self.currentEmail as Any])
 
                     self.emailSignInChecked = true
                     guard let respData = data as? [String: Any],
@@ -424,10 +419,7 @@ class UserAccountViewController: UIViewController {
                     if let user {
                         Lbryio.currentUser = user
                         if let id = user.id {
-                            Analytics.setDefaultEventParameters([
-                                "user_id": id,
-                                "user_email": user.primaryEmail ?? "",
-                            ])
+                            Analytics.setDefaultEventParameters(["user_id": id])
                         }
 
                         self.requestInProgress = false
