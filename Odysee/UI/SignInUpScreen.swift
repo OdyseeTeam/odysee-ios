40ff855262e2ae57a126ce303d700256dd8b50c9
kt programs <ktprograms@gmail.com>
2026-02-03 22:12:38 +0800

    screen(sign in/up): finish UI and link behaviour to view model
    Use weak captures in closures to prevent retain cycles

--- Odysee/UI/SignInUpScreen.swift
+++ Odysee/UI/SignInUpScreen.swift
@@ -8,34 +8,239 @@
 import SwiftUI
 
 struct SignInUpScreen: View {
+    var showClose: Bool
     var close: () -> Void
-    let closeRole = if #available(iOS 26, *) {
-        ButtonRole.close
+    @ObservedObject var model: ViewModel
+
+    let closeRole: ButtonRole = if #available(iOS 26, *) {
+        .close
     } else {
-        ButtonRole.cancel
+        .cancel
     }
+
+    enum Field: Hashable {
+        case email
+        case password
+    }
+
+    @State private var signUp = true
+    @State private var passwordStep = false
+    @State private var emailVerification = false
+
+    @State private var email = ""
+    @State private var password = ""
+    @FocusState private var focusedField: Field?
 
     var body: some View {
         ZStack {
-            Text("Hello, World!")
+            GeometryReader { metrics in
+                VStack {
+                    Image("spaceman_white")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                        .padding(.bottom, 32)
 
-            Button("Close", systemImage: "xmark", role: closeRole, action: close)
-                .labelStyle(.iconOnly)
-                .padding(.trailing)
+                    Text(signUp ? "Join Odysee" : "Log In to Odysee")
+                        .padding(.bottom, 32)
+
+                    if !emailVerification {
+                        if signUp || !passwordStep {
+                            TextField("Email", text: $email)
+                                .multilineTextAlignment(.center)
+                                .frame(width: metrics.size.width * 2 / 3)
+                                .keyboardType(.emailAddress)
+                                .textContentType(.username)
+                                .submitLabel(.next)
+                                .focused($focusedField, equals: .email)
+                        }
+                        if signUp || passwordStep {
+                            SecureField("Password", text: $password)
+                                .multilineTextAlignment(.center)
+                                .frame(width: metrics.size.width * 2 / 3)
+                                .textContentType(signUp ? .newPassword : .password)
+                                .submitLabel(.done)
+                                .focused($focusedField, equals: .password)
+                                .onAppear {
+                                    if passwordStep {
+                                        focusedField = .password
+                                    }
+                                }
+                        }
+
+                        Button(
+                            signUp ? "Sign Up" :
+                                passwordStep ? "Sign In" : "Continue",
+                            action: submit
+                        )
+                        .padding(.top, 32)
+                        .buttonStyle(.borderedProminent)
+
+                        if !signUp && passwordStep {
+                            Button("Use magic link") {
+                                Task {
+                                    do {
+                                        if try await model.emailVerification(email: email) == .emailVerification {
+                                            emailVerification = true
+                                        }
+                                    } catch {
+                                        Helper.showError(error: error)
+                                    }
+                                }
+                            }
+                            .padding(.top, 32)
+                        }
+                    } else {
+                        Text(
+                            "We sent an email to the address you provided. Please click the link in the message to complete email verification and continue using Odysee."
+                        )
+                        .padding(.bottom, 32)
+
+                        HStack {
+                            Button("Resend Email") {
+                                Task {
+                                    do {
+                                        _ = try await model.emailVerification(email: email)
+                                    } catch {
+                                        Helper.showError(error: error)
+                                    }
+                                }
+                            }
+
+                            Spacer()
+
+                            Button("Start Over") {
+                                model.stopEmailVerificationWait()
+
+                                signUp = true
+                                passwordStep = false
+                                emailVerification = false
+                                email = ""
+                                password = ""
+                                focusedField = .email
+                            }
+                        }
+                        .frame(width: metrics.size.width * 4 / 5)
+                    }
+
+                    Spacer()
+
+                    if !emailVerification {
+                        Text(signUp ? "Already have an account?" : "Don't have an account?")
+                            .padding(.bottom, 32)
+                    }
+
+                    Button(signUp ? "Log In" : "Sign Up") {
+                        model.stopEmailVerificationWait()
+
+                        signUp = !signUp
+
+                        passwordStep = false
+                        emailVerification = false
+                        email = ""
+                        password = ""
+                        focusedField = .email
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .padding(.top)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-                .tint(.white)
+                .padding(.bottom, 32)
+            }
+
+            if showClose {
+                Button("Close", systemImage: "xmark", role: closeRole, action: close)
+                    .labelStyle(.iconOnly)
+                    .padding(.trailing)
+                    .padding(.top)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                    .tint(.white)
+            }
+
+            ProgressView()
+                .controlSize(.large)
+                .apply {
+                    if model.inProgress {
+                        $0
+                    } else {
+                        $0.hidden()
+                    }
+                }
         }
         .environment(\.colorScheme, .dark)
+        .textFieldStyle(.roundedBorder)
+        .autocorrectionDisabled()
+        .textInputAutocapitalization(.never)
+        .disabled(model.inProgress)
+        .onSubmit(submit)
         .background {
             Image("ua_background")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
         }
+        .ignoresSafeArea(.keyboard)
+    }
+
+    private func submit() {
+        Task<Void, Never> {
+            if signUp {
+                if email.isEmpty {
+                    focusedField = .email
+                } else if password.isEmpty {
+                    focusedField = .password
+                } else {
+                    focusedField = nil
+                    do {
+                        if try await model.signUp(email: email, password: password) == .emailVerification {
+                            emailVerification = true
+                        }
+                    } catch {
+                        Helper.showError(error: error)
+                    }
+                }
+            } else {
+                if !passwordStep {
+                    do throws(ViewModel.ContinueError) {
+                        switch try await model.emailContinue(email: email) {
+                        case .password:
+                            passwordStep = true
+                        case .emailVerification:
+                            emailVerification = true
+                        }
+                    } catch {
+                        focusedField = .email
+                        switch error {
+                        case .empty:
+                            break
+                        case .invalid,
+                             .emailVerification:
+                            Helper.showError(error: error)
+                        }
+                    }
+                } else {
+                    if password.isEmpty {
+                        focusedField = .password
+                    } else {
+                        do {
+                            if try await model.signIn(email: email, password: password) == .emailVerification {
+                                emailVerification = true
+                            }
+                        } catch {
+                            Helper.showError(error: error)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SignInUpScreen(close: {})
+    SignInUpScreen(
+        showClose: true,
+        close: {},
+        model: .init(
+            finish: {},
+            frRequestStarted: {}, frRequestFinished: {}
+        )
+    )
 }
