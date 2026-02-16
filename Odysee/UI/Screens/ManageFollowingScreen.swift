b943cf0b6c11b7b33ed46476719f9cbd19ca5345
kt programs <ktprograms@gmail.com>
2026-02-16 18:28:52 +0800

    screen(manage following): refreshable

--- Odysee/UI/Screens/ManageFollowingScreen.swift
+++ Odysee/UI/Screens/ManageFollowingScreen.swift
@@ -99,6 +99,13 @@ extension ManageFollowingScreen {
                         }
                     }
             }
+            .refreshable {
+                do {
+                    try await model.refresh()
+                } catch {
+                    Helper.showError(error: error)
+                }
+            }
             .searchable(text: $search)
             .navigationTitle("Followed Channels")
         }
