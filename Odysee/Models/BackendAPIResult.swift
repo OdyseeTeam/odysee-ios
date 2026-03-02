952fbbcae3f013434e85635af2ecb05eb397f71f
kt programs <ktprograms@gmail.com>
2026-03-03 00:05:01 +0800

    refactor(Methods): ChannelSignResult to BackendAPIResult

--- Odysee/Models/BackendAPIResult.swift
+++ Odysee/Models/BackendAPIResult.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+
+struct ChannelSignResult: Decodable {
+    var signature: String
+    var signingTs: String
+
+    enum CodingKeys: String, CodingKey {
+        case signature
+        case signingTs = "signing_ts"
+    }
+}
+
 typealias SyncHashResult = String
 
 struct SyncApplyResult: Decodable {
