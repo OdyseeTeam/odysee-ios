6a5a1e452fd35e541ec27a4a4ace901ee4e8b540
kt programs <ktprograms@gmail.com>
2026-03-03 14:06:40 +0800

    screen(youtube sync): finish UI and link behaviour to view model

--- Odysee/UI/Screens/YouTubeSyncScreen.swift
+++ Odysee/UI/Screens/YouTubeSyncScreen.swift
@@ -6,13 +6,148 @@
 //
 
 import SwiftUI
+import WebKit
+
+extension Optional {
+    /// Use this to expose an `Optional` as a `Binding<Bool>`
+    var bool: Bool {
+        get {
+            self != nil
+        }
+        set {
+            if newValue == false {
+                self = nil
+            }
+        }
+    }
+}
+
+extension YouTubeSyncScreen {
+    struct WebView: UIViewRepresentable {
+        typealias UIViewType = WKWebView
+
+        @Environment(\.dismiss) var dismiss
+
+        @ObservedObject var model: YouTubeSyncScreen.ViewModel
+
+        func makeUIView(context: Context) -> WKWebView {
+            let webView = WKWebView()
+            webView.customUserAgent = "Mozilla/5.0 AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari/604.1"
+            webView.navigationDelegate = context.coordinator
+            webView.allowsBackForwardNavigationGestures = true
+            return webView
+        }
+
+        func updateUIView(_ webView: WKWebView, context: Context) {
+            guard let url = model.setupOauthUrl,
+                  webView.url != url
+            else {
+                return
+            }
+
+            webView.load(URLRequest(url: url))
+        }
+
+        class Coordinator: NSObject, WKNavigationDelegate {
+            var parent: WebView
+
+            init(_ parent: WebView) {
+                self.parent = parent
+            }
+
+            func webView(
+                _ webView: WKWebView,
+                decidePolicyFor navigationAction: WKNavigationAction
+            ) async -> WKNavigationActionPolicy {
+                if let url = navigationAction.request.url,
+                   url.absoluteString.lowercased().starts(with: Setup.returnUrl)
+                {
+                    do {
+                        try parent.model.updateSetupReturnUrl(url)
+                    } catch {
+                        Helper.showError(error: error)
+                    }
+
+                    parent.dismiss()
+
+                    return .cancel
+                }
+
+                return .allow
+            }
+        }
+
+        func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+        }
+    }
+}
 
 struct YouTubeSyncScreen: View {
+    var close: () -> Void
+    var viewChannel: (_ name: String, _ claimId: String) -> Void
+    @ObservedObject var model: ViewModel
+
     var body: some View {
-        Text("YouTube Sync")
+        ZStack {
+            GeometryReader { metrics in
+                NavigationView {
+                    ScrollView {
+                        Group {
+                            if model.showStatus {
+                                Status(
+                                    viewChannel: viewChannel,
+                                    model: model
+                                )
+                            } else {
+                                Setup(
+                                    close: close,
+                                    model: model
+                                )
+                            }
+                        }
+                        .frame(
+                            maxWidth: .infinity,
+                            minHeight: metrics.size.height,
+                            alignment: .top
+                        )
+                    }
+                    .apply {
+                        if #available(iOS 16.4, *) {
+                            $0.scrollBounceBehavior(.basedOnSize)
+                        } else {
+                            $0
+                        }
+                    }
+                }
+                .navigationViewStyle(.stack)
+
+                NavigationLink(isActive: $model.setupOauthUrl.bool) {
+                    WebView(model: model)
+                } label: {
+                    EmptyView()
+                }
+                .hidden()
+            }
+
+            ProgressView()
+                .controlSize(.large)
+                .apply {
+                    if model.inProgress {
+                        $0
+                    } else {
+                        $0.hidden()
+                    }
+                }
+        }
+        .disabled(model.inProgress)
     }
 }
 
 #Preview {
-    YouTubeSyncScreen()
+    YouTubeSyncScreen(
+        close: {},
+        viewChannel: { _, _ in },
+        model: .init()
+    )
 }
