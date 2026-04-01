64ca95fa68e41bb34db0eb45f397b27867bda1d3
kt programs <ktprograms@gmail.com>
2026-04-01 12:50:47 +0800

    ui(publishes): remove all UIKit, view controller

--- Odysee/Controllers/Library/LibraryViewController.swift
+++ Odysee/Controllers/Library/LibraryViewController.swift
@@ -6,10 +6,8 @@
 //
 
 import FirebaseAnalytics
-import UIKit
 
 class LibraryViewController: UIViewController {
-    @IBOutlet var viewContainer: UIView!
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -26,9 +24,6 @@ class LibraryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        showPublishesView()
-        // Omly show publishes for now. In the future, allow the user to select between publishes / view history
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -46,30 +41,5 @@ class LibraryViewController: UIViewController {
             bottom: Helper.miniPlayerBottomWithTabBar(appDelegate: AppDelegate.shared))
     }
 
-    func showPublishesView() {
-        let vc = storyboard?.instantiateViewController(identifier: "publishes_vc") as! PublishesViewController
-        showViewController(vc)
     }
-
-    func showViewController(_ vc: UIViewController) {
-        for subview in viewContainer.subviews {
-            subview.removeFromSuperview()
-        }
-
-        vc.willMove(toParent: self)
-        viewContainer.addSubview(vc.view)
-        vc.view.frame = CGRect(x: 0, y: 0, width: viewContainer.bounds.width, height: viewContainer.bounds.height)
-        addChild(vc)
-        vc.didMove(toParent: self)
-    }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
 }
