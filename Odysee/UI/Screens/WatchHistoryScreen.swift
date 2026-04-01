5b636e656bcdf6bbda1f3127cd498da292bd04ba
kt programs <ktprograms@gmail.com>
2026-04-01 14:19:00 +0800

    project(library): implement screens
    Similar functionality: paginated claims, swipe to edit/delete
    
    Watch History uses returned lastPosition's rather than needing to fetch
    
    TODO(Consider in future): Extract pagination to helper, or use a library

--- Odysee/UI/Screens/WatchHistoryScreen.swift
+++ Odysee/UI/Screens/WatchHistoryScreen.swift
@@ -10,11 +10,111 @@ import SwiftUI
 struct WatchHistoryScreen: View {
     @ObservedObject var model: ViewModel
 
+    @State private var showingConfirmClear = false
+
     var body: some View {
-        Text("Watch History")
+        GeometryReader { metrics in
+            ZStack {
+                List {
+                    Group {
+                        if !model.refreshing {
+                            if model.claims.isEmpty {
+                                Image("spaceman_sad")
+                                    .resizable()
+                                    .scaledToFit()
+                                    // Image is roughly a square
+                                    .frame(
+                                        maxWidth: .infinity,
+                                        maxHeight: min(metrics.size.height / 2, metrics.size.width / 2),
+                                        alignment: .center
+                                    )
+                                    .accessibilityHidden(true)
+
+                                Text("Nothing here")
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            } else {
+                                Button("Clear History", role: .destructive) {
+                                    showingConfirmClear = true
+                                }
+                                .padding(.horizontal)
+                                .disabled(model.inProgress)
+                                .confirmationDialog(
+                                    "Watch history will be cleared from this device and your synced account.",
+                                    isPresented: $showingConfirmClear,
+                                    titleVisibility: .visible
+                                ) {
+                                    Button("Clear History", role: .destructive) {
+                                        Task<Void, Never> {
+                                            await model.clearHistory()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        ForEach(model.claims) { claim in
+                            Button {
+                                let vc = AppDelegate.shared.mainViewController?.storyboard?
+                                    .instantiateViewController(identifier: "file_view_vc") as! FileViewController
+                                vc.claim = claim
+
+                                AppDelegate.shared.mainNavigationController?.view.layer.add(
+                                    Helper.buildFileViewTransition(),
+                                    forKey: kCATransition
+                                )
+                                AppDelegate.shared.mainNavigationController?.pushViewController(vc, animated: false)
+                            } label: {
+                                ClaimListItem(claim: claim)
+                            }
+                        }
+                        .onDelete { deleteOffsets in
+                            Task<Void, Never> {
+                                await model.delete(firstFromOffset: deleteOffsets)
+                            }
+                        }
+
+                        if model.inProgress || model.isLastPage {
+                            Color.clear
+                        } else {
+                            Color.clear
+                                .onAppear {
+                                    Task<Void, Never> {
+                                        await model.loadPage()
+                                    }
+                                }
+                        }
+                    }
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(.init())
+                }
+                .listStyle(.plain)
+                .refreshable(action: model.refresh)
+
+                ProgressView()
+                    .controlSize(.large)
+                    .apply {
+                        if model.inProgress {
+                            $0
+                        } else {
+                            $0.hidden()
+                        }
+                    }
+            }
+        }
     }
 }
 
 #Preview {
     WatchHistoryScreen(model: .init())
+}
+
+#Preview {
+    WatchHistoryScreen(model: .init(claims: [
+        .init(
+            name: "claim",
+            signingChannelRef: .init(.init(
+                name: "channel"
+            ))
+        )
+    ]))
 }
--- Odysee/ViewModels/PublishesViewModel.swift
+++ Odysee/ViewModels/PublishesViewModel.swift
@@ -10,5 +10,75 @@ import Foundation
 extension PublishesScreen {
     @MainActor
     class ViewModel: ObservableObject {
+        static let pageSize = 20
+        private var page = 1
+
+        @Published private(set) var inProgress = false
+        @Published private(set) var refreshing = false
+
+        @Published private(set) var isLastPage = false
+        @Published private(set) var claims: [Claim]
+
+        init(claims: [Claim] = []) {
+            self.claims = claims
+        }
+
+        @Sendable func refresh() async {
+            refreshing = true
+            defer {
+                refreshing = false
+            }
+
+            page = 1
+            claims.removeAll(keepingCapacity: true)
+            await loadPage(indicateProgress: false)
+        }
+
+        @Sendable func loadPage(indicateProgress: Bool = true) async {
+            if indicateProgress {
+                inProgress = true
+            }
+            defer {
+                inProgress = false
+            }
+
+            do {
+                let uploads = try await BackendMethods.claimList.call(params: .init(
+                    claimType: [.stream],
+                    page: page,
+                    pageSize: Self.pageSize,
+                    resolve: true
+                ))
+
+                claims.append(contentsOf: uploads.items)
+                isLastPage = uploads.isLastPage
+
+                page += 1
+            } catch {
+                Helper.showError(error: error)
+            }
+        }
+
+        func delete(claim: Claim) async {
+            guard let claimId = claim.claimId else {
+                Helper.showError(message: "claim has nil claimId")
+                return
+            }
+
+            inProgress = true
+            defer {
+                inProgress = false
+            }
+
+            do {
+                _ = try await BackendMethods.streamAbandon.call(params: .init(
+                    claimId: claimId, blocking: true
+                ))
+
+                claims.removeAll(where: { $0.claimId == claim.claimId })
+            } catch {
+                Helper.showError(error: error)
+            }
+        }
     }
 }
