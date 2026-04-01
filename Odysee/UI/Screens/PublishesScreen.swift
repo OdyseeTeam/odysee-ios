da69f1166637d9306a2db60df3a19c2cac3e21b5
kt programs <ktprograms@gmail.com>
2026-04-01 14:19:00 +0800

    project(library): implement screens
    Similar functionality: paginated claims, swipe to edit/delete
    
    Watch History uses returned lastPosition's rather than needing to fetch
    
    TODO(Consider in future): Extract pagination to helper, or use a library

--- Odysee/UI/Screens/PublishesScreen.swift
+++ Odysee/UI/Screens/PublishesScreen.swift
@@ -10,8 +10,128 @@ import SwiftUI
 struct PublishesScreen: View {
     @ObservedObject var model: ViewModel
 
+    @State private var toDelete: Claim?
+
+    var deleteConfirmText: String {
+        if let title = toDelete?.titleOrName {
+            __("Are you sure you'd like to remove \"\(title)\"?")
+        } else {
+            __("Are you sure you'd like to remove this claim?")
+        }
+    }
+
     var body: some View {
-        Text("Publishes")
+        GeometryReader { metrics in
+            ZStack {
+                List {
+                    Group {
+                        if !model.refreshing {
+                            if model.claims.isEmpty {
+                                VStack {
+                                    Image("spaceman_sad")
+                                        .resizable()
+                                        .scaledToFit()
+                                        // Image is roughly a square
+                                        .frame(
+                                            maxHeight: min(metrics.size.height / 2, metrics.size.width / 2),
+                                            alignment: .center
+                                        )
+                                        .accessibilityHidden(true)
+
+                                    Text("No uploads")
+
+                                    Button("Upload Something New") {
+                                        let vc = AppDelegate.shared.mainViewController?.storyboard?
+                                            .instantiateViewController(
+                                                identifier: "publish_vc"
+                                            ) as! PublishViewController
+                                        AppDelegate.shared.mainNavigationController?
+                                            .pushViewController(vc, animated: true)
+                                    }
+                                    .foregroundColor(.accentColor)
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            }
+                        }
+
+                        ForEach(model.claims) { claim in
+                            Button {
+                                let vc = AppDelegate.shared.mainViewController?.storyboard?
+                                    .instantiateViewController(identifier: "file_view_vc") as! FileViewController
+                                vc.claim = claim
+
+                                AppDelegate.shared.mainNavigationController?.view.layer.add(
+                                    Helper.buildFileViewTransition(),
+                                    forKey: kCATransition
+                                )
+                                AppDelegate.shared.mainNavigationController?.pushViewController(vc, animated: false)
+                            } label: {
+                                ClaimListItem(claim: claim)
+                            }
+                            .swipeActions(edge: .leading) {
+                                Button {
+                                    let vc = AppDelegate.shared.mainViewController?.storyboard?
+                                        .instantiateViewController(identifier: "publish_vc") as! PublishViewController
+                                    vc.currentClaim = claim
+                                    AppDelegate.shared.mainNavigationController?.pushViewController(vc, animated: true)
+                                } label: {
+                                    Label("Edit", systemImage: "pencil")
+                                }
+                                .tint(.blue)
+                            }
+                            .swipeActions {
+                                Button {
+                                    toDelete = claim
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                                // TODO: Make this an accessible destructive action, but without prematurely removing from the list
+                                .tint(.red)
+                            }
+                        }
+                        .confirmationDialog(
+                            deleteConfirmText,
+                            isPresented: $toDelete.bool,
+                            titleVisibility: .visible
+                        ) {
+                            Button("Delete", role: .destructive) {
+                                // Capture before confirmationDialog clears toDelete binding
+                                if let toDelete {
+                                    Task<Void, Never> {
+                                        await model.delete(claim: toDelete)
+                                    }
+                                }
+                            }
+                        }
+
+                        if model.inProgress || model.isLastPage {
+                            Color.clear
+                        } else {
+                            Color.clear
+                                .onAppear {
+                                    Task<Void, Never> {
+                                        await model.loadPage()
+                                    }
+                                }
+                        }
+                    }
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(.init())
+                }
+                .listStyle(.plain)
+                .refreshable(action: model.refresh)
+
+                ProgressView()
+                    .controlSize(.large)
+                    .apply {
+                        if model.inProgress {
+                            $0
+                        } else {
+                            $0.hidden()
+                        }
+                    }
+            }
+        }
     }
 }
 
--- Odysee/ViewModels/WatchHistoryViewModel.swift
+++ Odysee/ViewModels/WatchHistoryViewModel.swift
@@ -10,5 +10,118 @@ import Foundation
 extension WatchHistoryScreen {
     @MainActor
     class ViewModel: ObservableObject {
+        static let pageSize = 20
+        private var page = 1
+
+        @Published private(set) var inProgress = false
+        @Published private(set) var refreshing = false
+
+        @Published private(set) var isLastPage = false
+        @Published private(set) var claims: [Claim]
+
+        init(claims: [Claim] = []) {
+            self.claims = claims
+        }
+
+        @Sendable func refresh() async {
+            refreshing = true
+            defer {
+                refreshing = false
+            }
+
+            page = 1
+            claims.removeAll(keepingCapacity: true)
+            await loadPage(indicateProgress: false)
+        }
+
+        @Sendable func loadPage(indicateProgress: Bool = true) async {
+            if indicateProgress {
+                inProgress = true
+            }
+            defer {
+                inProgress = false
+            }
+
+            do {
+                let viewHistory = try await AccountMethods.viewHistory.call(params: .init(
+                    page: page,
+                    pageSize: Self.pageSize
+                ))
+
+                let urls = viewHistory.items.compactMap {
+                    try? LbryUri.normalize(url: "lbry://\($0.claimName):\($0.claimId)")
+                }
+
+                let lastPositionClaimIds = Dictionary(
+                    viewHistory.items.map { ($0.claimId, $0.lastPosition) },
+                    uniquingKeysWith: { _, last in last }
+                )
+
+                let resolve = try await BackendMethods.resolve.call(params: .init(urls: urls))
+                claims.append(contentsOf:
+                    resolve.claims.values.sorted(
+                        like: viewHistory.items.map(\.claimId),
+                        keyPath: \.claimId,
+                        transform: \.self
+                    ).map { claim in
+                        var claim = claim
+
+                        if let claimId = claim.claimId,
+                           let lastPosition = lastPositionClaimIds[claimId]
+                        {
+                            claim.lastPosition = lastPosition
+                        }
+
+                        return claim
+                    }
+                )
+
+                isLastPage = viewHistory.isLastPage
+
+                page += 1
+            } catch {
+                Helper.showError(error: error)
+            }
+        }
+
+        func delete(firstFromOffset offsets: IndexSet) async {
+            guard let offset = offsets.first,
+                  claims.count > offset,
+                  let claimId = claims[offset].claimId
+            else {
+                Helper.showError(message: "claim has nil claimId")
+                return
+            }
+
+            inProgress = true
+            defer {
+                inProgress = false
+            }
+
+            do {
+                _ = try await AccountMethods.viewHistoryDelete.call(params: .init(
+                    claimId: claimId
+                ))
+
+                claims.remove(atOffsets: IndexSet(integer: offset))
+            } catch {
+                Helper.showError(error: error)
+            }
+        }
+
+        func clearHistory() async {
+            inProgress = true
+            defer {
+                inProgress = false
+            }
+
+            do {
+                _ = try await AccountMethods.viewHistoryDeleteAll.call(params: .init())
+
+                await refresh()
+            } catch {
+                Helper.showError(error: error)
+            }
+        }
     }
 }
