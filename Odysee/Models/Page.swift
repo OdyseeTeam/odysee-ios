c07311bb25410559dfe51d9691441f56f8326987
kt programs <ktprograms@gmail.com>
2026-04-01 12:44:21 +0800

    model(page): support Account API user/view_history last page

--- Odysee/Models/Page.swift
+++ Odysee/Models/Page.swift
@@ -20,14 +20,24 @@ struct Page<Item: Decodable>: Decodable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         items = try container.decodeIfPresent([Item].self, forKey: .items) ?? []
 
-        let page = try container.decodeIfPresent(Int.self, forKey: .page)
-        let totalPages = try container.decodeIfPresent(Int.self, forKey: .totalPages)
-        isLastPage = page == totalPages
+        if let page = try container.decodeIfPresent(Int.self, forKey: .page),
+           let totalPages = try container.decodeIfPresent(Int.self, forKey: .totalPages)
+        {
+            isLastPage = page == totalPages
+        } else if let hasMore = try container.decodeIfPresent(Bool.self, forKey: .hasMore) {
+            isLastPage = !hasMore
+        } else {
+            isLastPage = false
+        }
     }
 
     private enum CodingKeys: String, CodingKey {
         case items
         case page
+
+        /// Backend API
         case totalPages = "total_pages"
+        /// Account API `user/view_history`
+        case hasMore = "has_more"
     }
 }
