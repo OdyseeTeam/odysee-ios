a7b09898d98797903470826fc2ca9b3d1e971573
kt programs <ktprograms@gmail.com>
2026-06-02 23:00:49 +0800

    ui: channel picker

---
+++ Odysee/UI/Components/ChannelPicker.swift
@@ -0,0 +1,57 @@
+//
+//  ChannelPicker.swift
+//  Odysee
+//
+//  Created by Keith on 26/05/2026.
+//
+
+import SwiftUI
+
+struct ChannelPicker: View {
+    @Binding var channel: Claim
+
+    @State private var channels: [Claim]?
+
+    var body: some View {
+        if let channels {
+            Picker("Channel", selection: $channel) {
+                ForEach(channels) {
+                    Text($0.name ?? "")
+                        .tag($0)
+                }
+            }
+            .pickerStyle(.menu)
+        } else {
+            ProgressView()
+                .onAppear {
+                    Task {
+                        do {
+                            let claimList = try await BackendMethods.claimList.call(params: .init(
+                                claimType: [.channel],
+                                page: 1,
+                                pageSize: 999,
+                                resolve: true
+                            ))
+
+                            let channels = claimList.items.filter { $0.claimId != Claim.anonymous.claimId }
+                            Lbry.ownChannels = channels
+
+                            let defaultChannelId = await Wallet.shared.defaultChannelId
+                            channel = channels.first { $0.claimId == defaultChannelId } ?? Claim.anonymous
+
+                            self.channels = channels + [Claim.anonymous]
+                        } catch {
+                            Helper.showError(message: __("Error loading channels: \(error.localizedDescription)"))
+
+                            channel = Claim.anonymous
+                            channels = [channel]
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ChannelPicker(channel: .constant(Claim.anonymous))
+}
